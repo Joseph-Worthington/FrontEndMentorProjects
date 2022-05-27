a25ea1f19244da554aa709da20c2eaa808ae87a6
Joe Worthington <joe@authenticity.digital>
2022-05-27 15:31:31 +0100

    Planning out solution

---
+++ Project 1/main.js
@@ -0,0 +1,17 @@
+let submitFrom = () =>{
+    onclick()
+}
+
+//Main function on click the current output will be hidden and the thank you message will become visable.  
+
+//Create an onclick event that targets the display of wrappers around the submit card and the thank you card.
+
+//The function must also log the number chosen, this must be stored.
+
+// How does the computer know which option was selected?
+
+// Do the buttons need an onlcick, or a change event?
+
+//The stored numebr then needs to be outputed into the html of the thank you message
+
+//Might also wish to include a return button
